5cf249e7fcf738e3a30dae38208236f044112947
Kravchuk <majerkravchuk@gmail.com>
2018-05-22 16:59:46 +0300

    Increase the speed of getting new proxies by using threads

--- app/jobs/upwork/fetch_proxies_job.rb
+++ app/jobs/upwork/fetch_proxies_job.rb
@@ -1,26 +1,36 @@
+require './app/models/upwork/job'
+
 class Upwork::FetchProxiesJob < Upwork::BaseJob
   queue_as :upwork_proxies
 
   def perform
     response = Net::HTTP.get(URI.parse('https://www.sslproxies.org'))
     page = Nokogiri::HTML(response)
-    rows = page.xpath('//*[@id="proxylisttable"]/tbody').children
+    children = page.xpath('//*[@id="proxylisttable"]/tbody').children
 
-    log(:info, "Got new proxies: #{rows.count}")
-    rows.each do |row|
-      proxy = Upwork::Proxy.find_or_create_by(
-        host: row.children[0].text,
-        port: row.children[1].text.to_i
-      )
+    proxies = []
 
-      if Net::Ping::TCP.new(proxy.host, proxy.port).ping?
-        if proxy.new_record?
-          proxy.save
-          log(:info, "#{proxy.host}:#{proxy.port}".yellow + ' created'.green)
-        end
+    threads = children.map do |child|
+      Thread.new(child) do |row|
+        proxy = Upwork::Proxy.new(
+          host: row.children[0].text,
+          port: row.children[1].text.to_i
+        )
+
+        proxy.state = Net::Ping::TCP.new(proxy.host, proxy.port).ping? ? :alive : :dead
+        proxies << proxy
+      end
+    end
+
+    threads.each { |thr| thr.join }
+
+    proxies.each do |proxy|
+      existed = Upwork::Proxy.find_by(host: proxy.host, port: proxy.port)
+
+      if existed.present?
+        existed.update(state: :alive) if existed.dead? && proxy.alive?
       else
-        proxy.dead!
-        log(:error, "#{proxy.host}:#{proxy.port}".yellow + ' is dead'.red)
+        proxy.save
       end
     end
   end
