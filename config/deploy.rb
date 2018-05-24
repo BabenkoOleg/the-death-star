4d2a6b2a1670b73efba4e87037d3f79f40babab9
Kravchuk <majerkravchuk@gmail.com>
2018-05-24 16:48:06 +0300

    Remove database.yml from shared_files

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :branch, 'master'
 # Don't forget to add a path to the uploads folder if you are using Dragonfly or CarrierWave.
 # Otherwise, you will lose your uploads on each deploy.
 set :shared_dirs, fetch(:shared_dirs, []).push('log', 'tmp/pids', 'tmp/sockets', 'public/uploads')
-set :shared_files, fetch(:shared_files, []).push('config/application.yml', 'config/database.yml', 'config/master.key', 'config/puma.rb')
+set :shared_files, fetch(:shared_files, []).push('config/application.yml', 'config/master.key', 'config/puma.rb')
 
 # Username of ssh user for access to the remote server.
 set :user, 'darth_vader'
@@ -53,7 +53,6 @@ task :setup do
   command %(mkdir -p "#{fetch(:deploy_to)}/shared/log/")
   command %(mkdir -p "#{fetch(:deploy_to)}/shared/public/uploads/")
 
-  command %(touch "#{fetch(:deploy_to)}/shared/config/database.yml")
   command %(touch "#{fetch(:deploy_to)}/shared/config/secrets.yml")
 end
 
@@ -65,7 +64,7 @@ task :deploy do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
     invoke :'git:clone'
-    # invoke :'sidekiq:quiet'
+    invoke :'sidekiq:quiet'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
