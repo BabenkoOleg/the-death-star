0d07c69d8af17bf7a568b14e3242df8bd8a5eb59
Kravchuk <majerkravchuk@gmail.com>
2018-05-25 16:07:05 +0300

    Fix Upwork::MasterJob

--- app/jobs/upwork/master_job.rb
+++ app/jobs/upwork/master_job.rb
@@ -2,7 +2,7 @@ class Upwork::MasterJob < Upwork::BaseJob
   queue_as :upwork_master
 
   def perform
-    Upwork::User.free.where(blocked: false).each do |user|
+    Upwork::User.free.where(locked: false).each do |user|
       Upwork::FetchJobAndClientInformationJob.perform_later(user.id)
       log(:info, "#{user.email.yellow} hired for work")
     end
